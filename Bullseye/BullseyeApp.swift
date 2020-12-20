57039e70a76a69b245307f2718b7c87ada738296
jakubzzak <zak.jakub67@gmail.com>
2020-12-20 22:37:08 +0100

    Initial Commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Jakub Žák on 20/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
